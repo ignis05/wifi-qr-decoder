3b3cb0b0c9acd423cf923ca2f69e55b252c9d1f5
ignis05 <48099798+ignis05@users.noreply.github.com>
2023-03-03 13:33:18 +0100

    feat: add bootstrap and icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
+
+import { MdCameraAlt, MdImage } from 'react-icons/md'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Container>
+			<Row>
+				<Col>
+					<h1>Wifi QR-code decoder</h1>
+					<p>
+						Android won't let you view saved wifi password.
+						<br />
+						But it lets you share it with QR-code, that can easily be decoded.
+					</p>
+				</Col>
+			</Row>
+			<Row>
+				<Col>
+					<Button variant="outline-primary" size="lg">
+						<MdCameraAlt size={30} /> Decode from camera
+					</Button>
+				</Col>
+				<Col>
+					<Button variant="outline-primary" size="lg">
+						<MdImage size={30} /> Decode from image
+					</Button>
+				</Col>
+			</Row>
+		</Container>
+	)
 }
 
-export default App;
+export default App
